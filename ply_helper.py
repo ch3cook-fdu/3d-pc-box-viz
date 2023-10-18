82db55d6cc2cd7c2ce4a37debdf32556974fae84
Sijin Chen <74592824+ch3cook-fdu@users.noreply.github.com>
2023-10-19 03:02:03 +0800

    Update ply_helper.py

--- ply_helper.py
+++ ply_helper.py
@@ -50,74 +50,75 @@ def write_ply(verts, colors, indices, output_file):
     file.close()
 
 
+def create_cylinder_mesh(radius, p0, p1, stacks=10, slices=10):
+    
+    import math
+
+    def compute_length_vec3(vec3):
+        return math.sqrt(vec3[0]*vec3[0] + vec3[1]*vec3[1] + vec3[2]*vec3[2])
+    
+    def rotation(axis, angle):
+        rot = np.eye(4)
+        c = np.cos(-angle)
+        s = np.sin(-angle)
+        t = 1.0 - c
+        axis /= compute_length_vec3(axis)
+        x = axis[0]
+        y = axis[1]
+        z = axis[2]
+        rot[0,0] = 1 + t*(x*x-1)
+        rot[0,1] = z*s+t*x*y
+        rot[0,2] = -y*s+t*x*z
+        rot[1,0] = -z*s+t*x*y
+        rot[1,1] = 1+t*(y*y-1)
+        rot[1,2] = x*s+t*y*z
+        rot[2,0] = y*s+t*x*z
+        rot[2,1] = -x*s+t*y*z
+        rot[2,2] = 1+t*(z*z-1)
+        return rot
+
+
+    verts = []
+    indices = []
+    diff = (p1 - p0).astype(np.float32)
+    height = compute_length_vec3(diff)
+    for i in range(stacks+1):
+        for i2 in range(slices):
+            theta = i2 * 2.0 * math.pi / slices
+            pos = np.array([radius*math.cos(theta), radius*math.sin(theta), height*i/stacks])
+            verts.append(pos)
+    for i in range(stacks):
+        for i2 in range(slices):
+            i2p1 = math.fmod(i2 + 1, slices)
+            indices.append( np.array([(i + 1)*slices + i2, i*slices + i2, i*slices + i2p1], dtype=np.uint32) )
+            indices.append( np.array([(i + 1)*slices + i2, i*slices + i2p1, (i + 1)*slices + i2p1], dtype=np.uint32) )
+    transform = np.eye(4)
+    va = np.array([0, 0, 1], dtype=np.float32)
+    vb = diff
+    vb /= compute_length_vec3(vb)
+    axis = np.cross(vb, va)
+    angle = np.arccos(np.clip(np.dot(va, vb), -1, 1))
+    if angle != 0:
+        if compute_length_vec3(axis) == 0:
+            dotx = va[0]
+            if (math.fabs(dotx) != 1.0):
+                axis = np.array([1,0,0]) - dotx * va
+            else:
+                axis = np.array([0,1,0]) - va[1] * va
+            axis /= compute_length_vec3(axis)
+        transform = rotation(axis, -angle)
+    transform[:3,3] += p0
+    verts = [np.dot(transform, np.array([v[0], v[1], v[2], 1.0])) for v in verts]
+    verts = [np.array([v[0], v[1], v[2]]) / v[3] for v in verts]
+        
+    return verts, indices
+
 def write_bbox(corners, color, output_file):
     """
     bbox: (cx, cy, cz, lx, ly, lz, r), center and length in three axis, the last is the rotation
     output_file: string
     """
-    def create_cylinder_mesh(radius, p0, p1, stacks=10, slices=10):
-        
-        import math
-
-        def compute_length_vec3(vec3):
-            return math.sqrt(vec3[0]*vec3[0] + vec3[1]*vec3[1] + vec3[2]*vec3[2])
-        
-        def rotation(axis, angle):
-            rot = np.eye(4)
-            c = np.cos(-angle)
-            s = np.sin(-angle)
-            t = 1.0 - c
-            axis /= compute_length_vec3(axis)
-            x = axis[0]
-            y = axis[1]
-            z = axis[2]
-            rot[0,0] = 1 + t*(x*x-1)
-            rot[0,1] = z*s+t*x*y
-            rot[0,2] = -y*s+t*x*z
-            rot[1,0] = -z*s+t*x*y
-            rot[1,1] = 1+t*(y*y-1)
-            rot[1,2] = x*s+t*y*z
-            rot[2,0] = y*s+t*x*z
-            rot[2,1] = -x*s+t*y*z
-            rot[2,2] = 1+t*(z*z-1)
-            return rot
-
-
-        verts = []
-        indices = []
-        diff = (p1 - p0).astype(np.float32)
-        height = compute_length_vec3(diff)
-        for i in range(stacks+1):
-            for i2 in range(slices):
-                theta = i2 * 2.0 * math.pi / slices
-                pos = np.array([radius*math.cos(theta), radius*math.sin(theta), height*i/stacks])
-                verts.append(pos)
-        for i in range(stacks):
-            for i2 in range(slices):
-                i2p1 = math.fmod(i2 + 1, slices)
-                indices.append( np.array([(i + 1)*slices + i2, i*slices + i2, i*slices + i2p1], dtype=np.uint32) )
-                indices.append( np.array([(i + 1)*slices + i2, i*slices + i2p1, (i + 1)*slices + i2p1], dtype=np.uint32) )
-        transform = np.eye(4)
-        va = np.array([0, 0, 1], dtype=np.float32)
-        vb = diff
-        vb /= compute_length_vec3(vb)
-        axis = np.cross(vb, va)
-        angle = np.arccos(np.clip(np.dot(va, vb), -1, 1))
-        if angle != 0:
-            if compute_length_vec3(axis) == 0:
-                dotx = va[0]
-                if (math.fabs(dotx) != 1.0):
-                    axis = np.array([1,0,0]) - dotx * va
-                else:
-                    axis = np.array([0,1,0]) - va[1] * va
-                axis /= compute_length_vec3(axis)
-            transform = rotation(axis, -angle)
-        transform[:3,3] += p0
-        verts = [np.dot(transform, np.array([v[0], v[1], v[2], 1.0])) for v in verts]
-        verts = [np.array([v[0], v[1], v[2]]) / v[3] for v in verts]
-            
-        return verts, indices
-
+    
     def get_bbox_edges(bbox_min, bbox_max):
         def get_bbox_verts(bbox_min, bbox_max):
             verts = [
@@ -172,3 +173,26 @@ def write_bbox(corners, color, output_file):
         colors.extend(cyl_color)
 
     write_ply(verts, colors, indices, output_file)
+    return
+
+
+def write_path(points, color, output_file):
+    
+    radius = 0.03
+    offset = [0,0,0]
+    verts = []
+    indices = []
+    colors = []
+    
+    for start, end in zip(points[:-1], points[1:]):
+        cyl_verts, cyl_ind = create_cylinder_mesh(radius, start, end)
+        cur_num_verts = len(verts)
+        cyl_color = [[c / 255 for c in color] for _ in cyl_verts]
+        cyl_verts = [x + offset for x in cyl_verts]
+        cyl_ind = [x + cur_num_verts for x in cyl_ind]
+        verts.extend(cyl_verts)
+        indices.extend(cyl_ind)
+        colors.extend(cyl_color)
+
+    write_ply(verts, colors, indices, output_file)
+    return
